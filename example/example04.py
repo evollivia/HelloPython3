53cbe2f96f34eb0df16077d4d2d19d81246887f8
evollivia <ytafxt@naver.com>
2024-07-16 15:10:05 +0900

    실습문제 풀이 보완 - 문자열 슬라이스

--- example/example04.py
+++ example/example04.py
@@ -9,3 +9,20 @@ for i in range(1, 10):
     for j in range(1, 10):
         print(f'  {j * i:2d}', end=' ')
     print()
+
+# 33
+cardno = input('카드번호 : ')
+result = '잘못된 카드번호입니다'
+if cardno[:2] == '35':
+    if cardno == '356317': result = 'JCB카드 NH농협카드'
+    elif cardno == '356901': result = 'JCB카드 신한카드'
+    elif cardno == '356912': result = 'JCB카드 KB국민카드'
+elif cardno[:1] == '4':
+    if cardno == '404825': result = '비자카드 비씨카드'
+    elif cardno == '438676': result = '비자카드 신한카드'
+    elif cardno == '457973': result = '비자카드 국민은행'
+elif cardno[:1] == '5':
+    if cardno == '515594': result = '마스터카드 신한카드'
+    elif cardno == '524353': result = '마스터카드 외환카드'
+    elif cardno == '5540926': result = '마스터카드 국민은행'
+print(f'{cardno} / {result}')
