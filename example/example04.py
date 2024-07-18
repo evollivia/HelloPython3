f17fe78e92b7275e7a79b6e2124675a05e3b4e95
evollivia <ytafxt@naver.com>
2024-07-18 14:00:26 +0900

    실습문제 추가

--- example/example04.py
+++ example/example04.py
@@ -36,6 +36,7 @@ elif cardno[:1] == '5':
         result = '마스터카드 국민은행'
 print(f'{cardno} / {result}')
 
+
 #############################################################
 # 16 개선하기 - 리스트, 반복문, 함수
 def compute_charge(price, received):
@@ -51,6 +52,7 @@ def compute_charge(price, received):
 received = int(input('받은금액을 입력하시오 : '))
 price = int(input('제품금액을 입력하시오 : '))
 compute_charge(price, received)
+
 
 ##########################################################
 def compute_charge(price, received):
@@ -73,7 +75,49 @@ def compute_charge(price, received):
 
     print(result)
 
+
 # 잔돈 구하는 함수 호출 및 테스트
 received = int(input('받은금액을 입력하시오 : '))
 price = int(input('제품금액을 입력하시오 : '))
 compute_charge(price, received)
+
+
+############################################################
+
+# 26. 세금 계산 computeTax
+def computeTax(isMarried, salary):
+    tax = 0
+    if isMarried == 0:
+        tax = salary * 0.1
+        if salary >= 3000:
+            tax = salary * 0.25
+    elif isMarried == 1:
+        tax = salary * 0.15
+        if salary >= 6000:
+            tax = salary * 0.35
+    print(f'''
+    결혼여부 : {isMarried}
+    연봉 : {salary}
+    세금 : {tax:,}''')
+
+
+isMarried = int(input('결혼여부 0:미혼, 1:기혼'))
+salary = int(input('연봉 : '))
+computeTax(isMarried, salary)
+
+
+# 27. 윤년 구분 isLeapYear
+# 1992, 2000, 2020 (윤O)
+# 1900, 2100 (윤X)
+def isLeapYear(year):
+    isleap = '윤년아님'
+    con1 = year % 4 == 0 and year % 100 != 0
+    con2 = year % 400 == 0
+
+    if con1 or con2:
+        isleap = '윤년맞음'
+    print(f'{year}년은 {isleap}')
+
+
+year = int(input('연도 : '))
+isLeapYear(year)
